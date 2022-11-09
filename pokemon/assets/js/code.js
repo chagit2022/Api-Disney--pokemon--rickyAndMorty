56f0c4e4eee3897ccb151e2017bdac84ad216a5c
chagit2022 <chadoutaud.git@outlook.fr>
2022-11-09 09:15:11 -0500

    download folders

---
+++ pokemon/assets/js/code.js
@@ -0,0 +1,36 @@
+async function consumoApi(urlAPI) {
+    let consumo = await fetch(urlAPI)
+    if (consumo.ok) {
+        return consumo.json()
+    }
+}
+
+async function procesarInfo(urlAPI, busqueda = false) {
+    if (busqueda) {
+        try {
+            document.querySelector("#grilla-pokemon").innerHTML = ``
+            let dataApi = await consumoApi(urlAPI)
+            document.querySelector("#grilla-pokemon").innerHTML += `
+                    <h1>${dataApi.name}</h1>`
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        try {
+            let dataApi = await consumoApi(urlAPI)
+            for (const e of dataApi.results) {
+                document.querySelector("#grilla-pokemon").innerHTML += `
+                    <h1>${e.name}</h1>`
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+procesarInfo("https://pokeapi.co/api/v2/pokemon")
+
+document.querySelector('#btnBusqueda').addEventListener('click', () => {
+    let campo = document.querySelector("#busquedaCampo").value
+    procesarInfo(`https://pokeapi.co/api/v2/pokemon/${campo}`, true)
+})
